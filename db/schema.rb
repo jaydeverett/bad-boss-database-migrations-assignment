08f1e1a27e19f58a7121efaea36e7b2f974af50e
Jay Deverett <jayjdeverett@gmail.com>
2017-07-24 15:09:23 -0400

    re-migrate quantity to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724185444) do
+ActiveRecord::Schema.define(version: 20170724190316) do
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
     t.string  "manufacturer"
     t.decimal "cost"
+    t.integer "quantity"
   end
 
 end
