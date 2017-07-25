937806b19888727a02a3a8d27aabb8f1c32ea73f
Jay Deverett <jayjdeverett@gmail.com>
2017-07-24 22:23:12 -0400

    add failed reversible method to change quantity to decimal - change took permanent effect

--- db/migrate/20170725000331_change_quantity_from_integer_to_decimal.rb
+++ db/migrate/20170725000331_change_quantity_from_integer_to_decimal.rb
@@ -1,14 +1,15 @@
 class ChangeQuantityFromIntegerToDecimal < ActiveRecord::Migration[5.0]
   def change
+
   end
 
   reversible do |dir|
     dir.up do
-        change_column_default :parts, :quantity, :decimal
+        change_column :parts, :quantity, :decimal
       end
 
       dir.down do
-        change_column_default :parts, :quantity, :integer
+        change_column :parts, :quantity, :integer
       end
-      end
+    end
 end
